add4b55630753bb6245ad31c8a79967b04b223ee
Sergieo <sergieol23@gmail.com>
2022-04-21 00:25:39 -0500

    Added fron-end functionality

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const {animals} = require('./data/animals.json');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // parse incoming string or arrary data
 app.use(express.urlencoded({extended: true}));
 // parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -119,6 +123,22 @@ if (!validateAnimal(req.body)) {
 
   }
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+})
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+})
+
+app.get('*', (res, req) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
